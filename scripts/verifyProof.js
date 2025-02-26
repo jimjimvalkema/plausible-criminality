f3621d7e4ed205629c142a16eb0256e654fb1375
jossduff <jduff360@gmail.com>
2025-02-26 12:52:12 -0500

    started script to verify proof

---
+++ scripts/verifyProof.js
@@ -0,0 +1,4 @@
+import { compile, createFileManager } from "@noir-lang/noir_js"
+
+import main from "../circuits/privateTransfer/src/main.nr?url";
+import nargoToml from "../circuits/privateTransfer/Nargo.toml?url";
